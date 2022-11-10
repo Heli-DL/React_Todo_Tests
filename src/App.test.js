ed095fc032ffda312debd6904a5f3c32a2cc55f9
Heli-DL <heli.delorme@outlook.com>
2022-11-10 19:19:15 +0200

    First commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import'@testing-library/jest-dom/extend-expect';
+import TodoTable from'./TodoTable';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders todotable', () => {
+  const row = [    
+    {desc: 'Go to coffee', date: '11.09.2022'}  
+  ];  
+  render(<TodoTable todos={row} />);
+  const tablecell = screen.getByText(/go to coffee/i);  
+  expect(tablecell).toBeInTheDocument();
+});
+
+test('add todo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const desc = screen.getByPlaceholderText('Description');
+  fireEvent.change(desc, { target: { value: 'Go to coffee' } });
+  const date = screen.getByPlaceholderText('Date');
+  fireEvent.change(date, { target: { value: '11.09.2022' } });
+  const button = screen.getByText('Add');
+  fireEvent.click(button);
+  const tablecell = screen.getByText(/go to coffee/i);
+  expect(tablecell).toBeInTheDocument();
+});
+
+test('clear todos', () => {
+  render(<App />);
+  const desc = screen.getByPlaceholderText('Description');
+  fireEvent.change(desc, { target: { value: 'Go to coffee' } });
+  const date = screen.getByPlaceholderText('Date');
+  fireEvent.change(date, { target: { value: '11.09.2022' } });
+  const button = screen.getByText('Add');
+  fireEvent.click(button);
+  const button2 = screen.getByText('Clear');
+  fireEvent.click(button2);
+  const tablecell = screen.queryByText(/go to coffee/i);
+  expect(tablecell).not.toBeInTheDocument();
 });
